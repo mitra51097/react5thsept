dfd39caf61e6e32b10ff07ceb38087d88c3ab8ba
Navyateja Cheerladinne <navyateja.cheerladinne@zensar.com>
2022-09-09 15:43:55 +0530

    Canonical text

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          ReactDummy
+          AfterAddingCanonicalUserInBucketPolicy
         </a>
       </header>
     </div>
