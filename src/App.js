ca1c7d582178418a98c96626bc7fb654ac426d72
Navyateja Cheerladinne <navyateja.cheerladinne@zensar.com>
2022-09-12 16:42:12 +0530

    1209_2.0

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          reactdemo1209
+          reactdemo1209_2.0
         </a>
       </header>
     </div>
